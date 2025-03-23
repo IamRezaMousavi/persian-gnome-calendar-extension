4b470aa265da89284391b47bb554a83dbd6b0ed0
Reza Mousavi <iamrezamousavi@gmail.com>
2025-03-23 08:49:37 +0330

    migrating to eslint new config file

--- persian-calendar@iamrezamousavi.gmail.com/dateMenu.js
+++ persian-calendar@iamrezamousavi.gmail.com/dateMenu.js
@@ -209,7 +209,7 @@ class EventsSection extends St.Button {
         Main.panel.closeCalendar();
 
         const appInfo = this._calendarApp;
-        // eslint-disable-next-line no-undef
+
         const context = global.create_app_launch_context(0, -1);
         if (appInfo.get_id() === 'org.gnome.Evolution.desktop')
             appInfo.launch_action('calendar', context);
